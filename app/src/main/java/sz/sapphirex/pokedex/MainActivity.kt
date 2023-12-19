e03923501fc2a4bd5ea2ce5c25d4d3c82ec547fd
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-20 00:27:21 +0800

    kek

---
+++ app/src/main/java/sz/sapphirex/pokedex/MainActivity.kt
@@ -0,0 +1,17 @@
+package sz.sapphirex.pokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import sz.sapphirex.pokedex.presentation.theme.PokedexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokedexTheme {
+
+            }
+        }
+    }
+}
