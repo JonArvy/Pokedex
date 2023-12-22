6e29dd2f9351e4711f63080e96f1c810c3c29edd
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-22 23:19:22 +0800

    install hilt and view

--- app/src/main/java/sz/sapphirex/pokedex/presentation/MainActivity.kt
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/MainActivity.kt
@@ -3,14 +3,22 @@ package sz.sapphirex.pokedex.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.navigator.Navigator
+import dagger.hilt.android.AndroidEntryPoint
+import sz.sapphirex.pokedex.presentation.screens.home.HomeScreen
 import sz.sapphirex.pokedex.presentation.theme.PokedexTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
-
+                Navigator(HomeScreen())
             }
         }
     }
