e081dc9888c2ce5e4c781e358482c496d71b0311
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-22 23:17:22 +0800

    remove db init from application class

--- app/src/main/java/sz/sapphirex/pokedex/App.kt
+++ app/src/main/java/sz/sapphirex/pokedex/App.kt
@@ -1,19 +1,9 @@
 package sz.sapphirex.pokedex
 
 import android.app.Application
-import androidx.room.Room
 import dagger.hilt.android.HiltAndroidApp
-import sz.sapphirex.pokedex.data.database.PokemonDao
-import sz.sapphirex.pokedex.data.database.PokemonDatabase
 
 @HiltAndroidApp
 class App: Application() {
-    private val database: PokemonDatabase by lazy {
-        Room.databaseBuilder(this, PokemonDatabase::class.java, "app_database").build()
-    }
-
-    val pokemonDao: PokemonDao by lazy {
-        database.pokemonDao()
-    }
 
 }
