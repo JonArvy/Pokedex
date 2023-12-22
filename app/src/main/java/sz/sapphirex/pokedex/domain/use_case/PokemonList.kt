b0c86a4e79b2ada88c059348d0d43aa0dca19fc3
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-22 23:20:58 +0800

    repository implementation operator functions

---
+++ app/src/main/java/sz/sapphirex/pokedex/domain/use_case/PokemonList.kt
@@ -0,0 +1,25 @@
+package sz.sapphirex.pokedex.domain.use_case
+
+import sz.sapphirex.pokedex.domain.model.utils.DataResult
+import sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon
+import kotlinx.coroutines.flow.Flow
+import sz.sapphirex.pokedex.domain.model.base.pokemon.PokemonType
+import sz.sapphirex.pokedex.domain.repository.PokemonListRepository
+import javax.inject.Inject
+
+class PokemonList @Inject constructor(
+    private val repository: PokemonListRepository
+) {
+    operator fun invoke(): Flow<DataResult<List<Pokemon>>> {
+        return repository.getPokemonList()
+    }
+
+    operator fun invoke(query: String): Flow<DataResult<List<Pokemon>>> {
+        return repository.searchPokemon(query)
+    }
+
+    operator fun invoke(pokemonTypes: List<PokemonType>): Flow<DataResult<List<Pokemon>>> {
+        return repository.getPokemonByTypes(pokemonTypes)
+    }
+
+}
