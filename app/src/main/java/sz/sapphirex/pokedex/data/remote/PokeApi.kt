01dfdc12c16c3be1c92433c8029d8f8d3228e461
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-23 23:29:56 +0800

    api dto classes implementation

--- app/src/main/java/sz/sapphirex/pokedex/data/remote/PokeApi.kt
+++ app/src/main/java/sz/sapphirex/pokedex/data/remote/PokeApi.kt
@@ -3,44 +3,44 @@ package sz.sapphirex.pokedex.data.remote
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import sz.sapphirex.pokedex.domain.model.base.pokemon.Ability
-import sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon
-import sz.sapphirex.pokedex.domain.model.base.pokemon.PokemonSpecies
-import sz.sapphirex.pokedex.domain.model.base.resource.Named
+import sz.sapphirex.pokedex.data.remote.dto.pokemon.AbilityDto
+import sz.sapphirex.pokedex.data.remote.dto.pokemon.PokemonDto
+import sz.sapphirex.pokedex.data.remote.dto.pokemon.PokemonSpeciesDto
+import sz.sapphirex.pokedex.data.remote.dto.resource.NamedDto
 import javax.inject.Inject
 
 class PokeApi @Inject constructor(
     private val httpClient: HttpClient
 ) {
     /******************** GENERIC ENDPOINT ********************/
-    suspend fun getDataByEndpoint(endpoint: String): Named {
-        return safeApiCall<Named>(endpoint)
+    suspend fun getDataByEndpoint(endpoint: String): NamedDto {
+        return safeApiCall<NamedDto>(endpoint)
     }
 
     /******************** POKEMON ********************/
-    suspend fun getPokemon(id: Int): Pokemon {
-        return safeApiCall<Pokemon>("${ApiEndpoints.ENDPOINT_POKEMON}$id/")
+    suspend fun getPokemon(id: Int): PokemonDto {
+        return safeApiCall<PokemonDto>("${ApiEndpoints.ENDPOINT_POKEMON}$id/")
     }
 
-    suspend fun getPokemon(name: String): Pokemon {
-        return safeApiCall<Pokemon>("${ApiEndpoints.ENDPOINT_POKEMON}$name/")
+    suspend fun getPokemon(name: String): PokemonDto {
+        return safeApiCall<PokemonDto>("${ApiEndpoints.ENDPOINT_POKEMON}$name/")
     }
 
-    suspend fun getPokemonAbility(id: Int): Ability {
-        return safeApiCall<Ability>("${ApiEndpoints.ENDPOINT_ABILITY}$id/")
+    suspend fun getPokemonAbility(id: Int): AbilityDto {
+        return safeApiCall<AbilityDto>("${ApiEndpoints.ENDPOINT_ABILITY}$id/")
     }
 
-    suspend fun getPokemonAbility(name: String): Ability {
-        return safeApiCall<Ability>("${ApiEndpoints.ENDPOINT_ABILITY}$name/")
+    suspend fun getPokemonAbility(name: String): AbilityDto {
+        return safeApiCall<AbilityDto>("${ApiEndpoints.ENDPOINT_ABILITY}$name/")
     }
 
     /******************** POKEMON SPECIES ********************/
-    suspend fun getPokemonSpecies(id: Int): PokemonSpecies {
-        return safeApiCall<PokemonSpecies>("${ApiEndpoints.ENDPOINT_POKEMON_SPECIES}$id/")
+    suspend fun getPokemonSpecies(id: Int): PokemonSpeciesDto {
+        return safeApiCall<PokemonSpeciesDto>("${ApiEndpoints.ENDPOINT_POKEMON_SPECIES}$id/")
     }
 
-    suspend fun getPokemonSpecies(name: String): PokemonSpecies {
-        return safeApiCall<PokemonSpecies>("${ApiEndpoints.ENDPOINT_POKEMON_SPECIES}$name/")
+    suspend fun getPokemonSpecies(name: String): PokemonSpeciesDto {
+        return safeApiCall<PokemonSpeciesDto>("${ApiEndpoints.ENDPOINT_POKEMON_SPECIES}$name/")
     }
 
     /******************** HELPER ********************/
