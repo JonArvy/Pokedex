e6f00f4537144c9d1da61a5d78b6841eda732124
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-23 23:05:13 +0800

    fix resource Dto classes

--- app/src/main/java/sz/sapphirex/pokedex/domain/model/dto/resource/NamedDto.kt
+++ app/src/main/java/sz/sapphirex/pokedex/domain/model/dto/resource/NamedDto.kt
@@ -3,15 +3,15 @@ package sz.sapphirex.pokedex.domain.model.dto.resource
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
-import sz.sapphirex.pokedex.domain.model.base.utility.NamedAPIResource
+import sz.sapphirex.pokedex.domain.model.dto.utility.NamedAPIResourceDto
 
 @Serializable
 @Entity(tableName = "named")
-data class Named(
+data class NamedDto(
     @PrimaryKey
     var id: String = "",
     val count: Int,
     val next: String?,
     val previous: String?,
-    val results: List<NamedAPIResource>
+    val results: List<NamedAPIResourceDto>
 )
