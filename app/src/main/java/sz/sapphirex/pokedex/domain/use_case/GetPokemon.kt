08f8f487e90bbb24fc5393be91088dd94462015a
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-23 23:32:12 +0800

    rename the class of repository operator

--- app/src/main/java/sz/sapphirex/pokedex/domain/use_case/GetPokemon.kt
+++ app/src/main/java/sz/sapphirex/pokedex/domain/use_case/GetPokemon.kt
@@ -1,6 +1,7 @@
 package sz.sapphirex.pokedex.domain.use_case
 
 import kotlinx.coroutines.flow.Flow
+import sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon
 import sz.sapphirex.pokedex.domain.model.utils.DataResult
 import sz.sapphirex.pokedex.domain.repository.PokemonRepository
 import javax.inject.Inject
@@ -8,11 +9,11 @@ import javax.inject.Inject
 class GetPokemon @Inject constructor(
     private val pokemonRepository: PokemonRepository
 ) {
-    operator fun invoke(name: String): Flow<DataResult<sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon>> {
+    operator fun invoke(name: String): Flow<DataResult<Pokemon>> {
         return pokemonRepository.getPokemon(name)
     }
 
-    operator fun invoke(id: Int): Flow<DataResult<sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon>> {
+    operator fun invoke(id: Int): Flow<DataResult<Pokemon>> {
         return pokemonRepository.getPokemon(id)
     }
 }
