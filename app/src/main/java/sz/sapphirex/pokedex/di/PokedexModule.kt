3b9fdb7eb2cbe34dda156b8ad4ed8f84770361f0
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-24 22:58:20 +0800

    implement type converter on database creation

--- app/src/main/java/sz/sapphirex/pokedex/di/PokedexModule.kt
+++ app/src/main/java/sz/sapphirex/pokedex/di/PokedexModule.kt
@@ -2,14 +2,17 @@ package sz.sapphirex.pokedex.di
 
 import android.content.Context
 import androidx.room.Room
+import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
+import sz.sapphirex.pokedex.data.local.Converters
 import sz.sapphirex.pokedex.data.local.PokemonDao
 import sz.sapphirex.pokedex.data.local.PokemonDatabase
+import sz.sapphirex.pokedex.data.local.util.GsonParser
 import sz.sapphirex.pokedex.data.remote.PokeApi
 import sz.sapphirex.pokedex.data.remote.ktorHttpClient
 import sz.sapphirex.pokedex.data.repository.PokemonListRepositoryImpl
@@ -34,7 +37,9 @@ object PokedexModule {
     @Provides
     @Singleton
     fun providePokemonDatabase(@ApplicationContext context: Context): PokemonDatabase {
-        return Room.databaseBuilder(context, PokemonDatabase::class.java, "app_database").build()
+        return Room.databaseBuilder(context, PokemonDatabase::class.java, "app_database")
+            .addTypeConverter(Converters(GsonParser(Gson())))
+            .build()
     }
 
     @Provides
