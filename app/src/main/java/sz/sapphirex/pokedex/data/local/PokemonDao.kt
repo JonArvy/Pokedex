daf3780a3905093d75e03f9921f69df371e018e1
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-24 22:58:58 +0800

    change dao data classes from base to entity data class

--- app/src/main/java/sz/sapphirex/pokedex/data/local/PokemonDao.kt
+++ app/src/main/java/sz/sapphirex/pokedex/data/local/PokemonDao.kt
@@ -4,31 +4,31 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon
-import sz.sapphirex.pokedex.domain.model.base.resource.Named
+import sz.sapphirex.pokedex.data.local.entity.pokemon.PokemonEntity
+import sz.sapphirex.pokedex.data.local.entity.resource.NamedEntity
 
 @Dao
 interface PokemonDao {
     /** Pokemon **/
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPokemon(pokemon: Pokemon)
+    suspend fun insertPokemon(pokemon: PokemonEntity)
 
     @Query("SELECT * FROM pokemon WHERE name = :name")
-    suspend fun getPokemon(name: String): Pokemon?
+    suspend fun getPokemon(name: String): PokemonEntity?
 
     @Query("SELECT * FROM pokemon")
-    suspend fun getAllPokemon(): List<Pokemon>
+    suspend fun getAllPokemon(): List<PokemonEntity>
 
     @Query("SELECT * FROM pokemon WHERE name IN(:nameList)")
-    suspend fun getPokemonListByName(nameList: List<String>): List<Pokemon>
+    suspend fun getPokemonListByName(nameList: List<String>): List<PokemonEntity>
 
     @Query("SELECT * FROM pokemon WHERE name IN(:idList)")
-    suspend fun getPokemonListById(idList: List<Int>): List<Pokemon>
+    suspend fun getPokemonListById(idList: List<Int>): List<PokemonEntity>
 
     /** Named **/
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertNamed(named: Named)
+    suspend fun insertNamed(named: NamedEntity)
 
     @Query("SELECT * FROM named WHERE id =:id")
-    suspend fun getNamed(id: String): Named?
+    suspend fun getNamed(id: String): NamedEntity?
 }
