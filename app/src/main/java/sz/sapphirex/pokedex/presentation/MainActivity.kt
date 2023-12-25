7800094c0cf8cf21463ceb9f26783e9584eebd12
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-25 23:40:12 +0800

    clean imports

--- app/src/main/java/sz/sapphirex/pokedex/presentation/MainActivity.kt
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/MainActivity.kt
@@ -1,14 +1,11 @@
 package sz.sapphirex.pokedex.presentation
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -16,14 +13,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import sz.sapphirex.pokedex.presentation.screens.abilities.AbilityTab
-import sz.sapphirex.pokedex.presentation.screens.home.HomeScreen
 import sz.sapphirex.pokedex.presentation.screens.home.HomeTab
 import sz.sapphirex.pokedex.presentation.screens.moves.MoveTab
 import sz.sapphirex.pokedex.presentation.screens.settings.SettingsTab
