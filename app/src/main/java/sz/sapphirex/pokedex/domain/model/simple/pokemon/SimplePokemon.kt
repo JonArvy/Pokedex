02811308524d0c1a26e51162e67b220f7a4b7012
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-25 23:12:35 +0800

    refactor simple pokemon and add typing for color coding

--- app/src/main/java/sz/sapphirex/pokedex/domain/model/simple/pokemon/SimplePokemon.kt
+++ app/src/main/java/sz/sapphirex/pokedex/domain/model/simple/pokemon/SimplePokemon.kt
@@ -1,19 +1,21 @@
 package sz.sapphirex.pokedex.domain.model.simple.pokemon
 
-import androidx.room.PrimaryKey
 import sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon
 import sz.sapphirex.pokedex.domain.model.base.pokemon.PokemonSprites
+import sz.sapphirex.pokedex.domain.model.base.pokemon.PokemonType
 
 data class SimplePokemon(
     val id: Int,
     val name: String,
     val sprites: PokemonSprites,
+    val types: List<PokemonType>
 )
 
 fun Pokemon.toSimple(): SimplePokemon {
     return SimplePokemon(
         id = id,
         name = name,
-        sprites = sprites
+        sprites = sprites,
+        types = types,
     )
 }
