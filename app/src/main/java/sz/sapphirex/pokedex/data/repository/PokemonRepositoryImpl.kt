4e34884fabd0093a844981b2de8e5d27a7f3fc10
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-25 23:11:22 +0800

    implement getting pokemon flow

--- app/src/main/java/sz/sapphirex/pokedex/data/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/sz/sapphirex/pokedex/data/repository/PokemonRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package sz.sapphirex.pokedex.data.repository
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import sz.sapphirex.pokedex.data.local.PokemonDao
 import sz.sapphirex.pokedex.data.remote.PokeApi
 import sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon
 import sz.sapphirex.pokedex.domain.model.utils.DataResult
 import sz.sapphirex.pokedex.domain.repository.PokemonRepository
+import java.io.IOException
 import javax.inject.Inject
 
 class PokemonRepositoryImpl @Inject constructor(
@@ -16,7 +18,19 @@ class PokemonRepositoryImpl @Inject constructor(
         TODO("Not yet implemented")
     }
 
-    override fun getPokemon(id: Int): Flow<DataResult<Pokemon>> {
-        TODO("Not yet implemented")
+    override fun getPokemon(id: Int): Flow<DataResult<Pokemon>> = flow {
+        emit(DataResult.Loading)
+        val cachePokemon = pokemonDao.getPokemon(id)
+        if (cachePokemon == null) {
+            try {
+                val apiPokemon = pokeApi.getPokemon(id)
+                emit(DataResult.Success(apiPokemon.toBase()))
+                pokemonDao.insertPokemon(apiPokemon.toBase().toEntity())
+            } catch (e: IOException) {
+                emit(DataResult.Error(e, "No Internet Connection"))
+            } catch (e: Exception) {
+                emit(DataResult.Error(e, e.message.toString()))
+            }
+        }
     }
 }
