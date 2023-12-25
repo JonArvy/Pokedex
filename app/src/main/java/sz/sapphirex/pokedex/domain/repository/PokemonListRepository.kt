67f6011fa00d6a64f4b3d2a810b9c4a1c8fb6628
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-25 23:15:47 +0800

    change return function to simple pokemon

--- app/src/main/java/sz/sapphirex/pokedex/domain/repository/PokemonListRepository.kt
+++ app/src/main/java/sz/sapphirex/pokedex/domain/repository/PokemonListRepository.kt
@@ -4,13 +4,12 @@ import sz.sapphirex.pokedex.domain.model.utils.DataResult
 import sz.sapphirex.pokedex.domain.model.base.pokemon.Pokemon
 import kotlinx.coroutines.flow.Flow
 import sz.sapphirex.pokedex.domain.model.base.pokemon.PokemonType
+import sz.sapphirex.pokedex.domain.model.simple.pokemon.SimplePokemon
 
 interface PokemonListRepository {
-    fun getPokemonList(): Flow<DataResult<List<Pokemon>>>
+    fun getPokemonList(): Flow<DataResult<List<SimplePokemon>>>
 
     fun searchPokemon(query: String): Flow<DataResult<List<Pokemon>>>
 
     fun getPokemonByTypes(pokemonTypes: List<PokemonType>): Flow<DataResult<List<Pokemon>>>
-
-
 }
