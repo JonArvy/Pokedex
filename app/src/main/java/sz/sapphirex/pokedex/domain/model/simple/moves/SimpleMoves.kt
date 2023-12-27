26976033fd09f91bcedc814752331f956db83c6b
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-27 23:50:10 +0800

    add simple move data class

---
+++ app/src/main/java/sz/sapphirex/pokedex/domain/model/simple/moves/SimpleMoves.kt
@@ -0,0 +1,9 @@
+package sz.sapphirex.pokedex.domain.model.simple.moves
+
+import sz.sapphirex.pokedex.domain.model.base.utility.NamedAPIResource
+
+data class SimpleMove(
+    val id: Int,
+    val name: String,
+    val type: NamedAPIResource
+)
