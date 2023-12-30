3709651f6e745790c5585e1050ed54bec4c9abad
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-30 22:42:34 +0800

    change return type

--- app/src/main/java/sz/sapphirex/pokedex/domain/repository/MoveListRepository.kt
+++ app/src/main/java/sz/sapphirex/pokedex/domain/repository/MoveListRepository.kt
@@ -1,11 +1,12 @@
 package sz.sapphirex.pokedex.domain.repository
 
+import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import sz.sapphirex.pokedex.domain.model.simple.moves.SimpleMove
 import sz.sapphirex.pokedex.domain.model.utils.DataResult
 
 interface MoveListRepository {
-    fun getMoveList(): Flow<DataResult<List<SimpleMove>>>
+    fun getMoveList(): Flow<DataResult<PagingData<SimpleMove>>>
 
     fun searchMove(query: String): Flow<DataResult<List<SimpleMove>>>
 }
