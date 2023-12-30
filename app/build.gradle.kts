c901b8e657f852141313431921c939e549f63d79
Arvy Enriquez <jonarvy11@gmail.com>
2023-12-30 22:39:00 +0800

    add paging library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,6 +108,9 @@ dependencies {
     implementation("cafe.adriel.voyager:voyager-transitions:$voyager")
     implementation("cafe.adriel.voyager:voyager-hilt:$voyager")
 
+    // Paging
+    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
