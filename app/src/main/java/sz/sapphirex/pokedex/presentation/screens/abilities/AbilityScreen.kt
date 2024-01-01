56fa8834eac703a33463c918b73b4ff027bf0a0e
Arvy Enriquez <jonarvy11@gmail.com>
2024-01-01 22:16:32 +0800

    ability

---
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/screens/abilities/AbilityScreen.kt
@@ -0,0 +1,11 @@
+package sz.sapphirex.pokedex.presentation.screens.abilities
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class AbilityScreen : Screen {
+    @Composable
+    override fun Content() {
+
+    }
+}
---
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/screens/abilities/AbilityScreenViewModel.kt
@@ -0,0 +1,4 @@
+package sz.sapphirex.pokedex.presentation.screens.abilities
+
+class AbilityScreenViewModel {
+}
