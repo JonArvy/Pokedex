8c7fd6bb623d39190ef815b691bb1d1a524b5657
Arvy Enriquez <jonarvy11@gmail.com>
2024-01-18 22:21:28 +0800

    clean homescreen to implement paging

--- app/src/main/java/sz/sapphirex/pokedex/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/screens/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package sz.sapphirex.pokedex.presentation.screens.home
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -7,14 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getViewModel
+import sz.sapphirex.pokedex.domain.model.simple.moves.SimpleMove
+import sz.sapphirex.pokedex.domain.model.simple.pokemon.SimplePokemon
 import sz.sapphirex.pokedex.domain.model.utils.DataResult
 import sz.sapphirex.pokedex.presentation.components.PokemonCard
 
@@ -22,27 +31,27 @@ class HomeScreen : Screen {
     @Composable
     override fun Content() {
         val homeScreenViewModel = getViewModel<HomeScreenViewModel>()
-        val pokemons by homeScreenViewModel.pokemons.collectAsState()
-        Box(modifier = Modifier.fillMaxSize()) {
-            Column {
-                when (val data = pokemons) {
-                    is DataResult.Success -> {
-                        val lazyState = rememberLazyGridState()
-                        LazyVerticalGrid(
-                            state = lazyState,
-                            columns = GridCells.Fixed(count = 2),
-                            contentPadding = PaddingValues(4.dp),
-                        ) {
-                            data.data.forEach {
-                                item {
-                                    PokemonCard(pokemon = it, padding = PaddingValues(4.dp))
-                                }
-                            }
-                        }
+        val pokemonData = homeScreenViewModel.pagedData.collectAsLazyPagingItems()
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            val lazyState = rememberLazyGridState()
+            LazyVerticalGrid(
+                state = lazyState,
+                columns = GridCells.Fixed(count = 2),
+                contentPadding = PaddingValues(4.dp),
+            ) {
+                items(
+                    count = pokemonData.itemCount,
+                    key = pokemonData.itemKey { data -> data.id },
+                    contentType = pokemonData.itemContentType { "Pokemons" },
+                ) {index ->
+                    val pokemon: SimplePokemon? = pokemonData[index]
+
+                    pokemon?.let {
+                        PokemonCard(pokemon = it, padding = PaddingValues(4.dp))
                     }
-                    is DataResult.Error -> Text(text = data.message)
-                    is DataResult.Loading -> Text(text = "Loading")
                 }
+                if (pokemonData.loadState.append == LoadState.Loading) item { CircularProgressIndicator() }
             }
         }
     }
--- app/src/main/java/sz/sapphirex/pokedex/presentation/screens/home/HomeScreenViewModel.kt
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/screens/home/HomeScreenViewModel.kt
@@ -2,10 +2,14 @@ package sz.sapphirex.pokedex.presentation.screens.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import sz.sapphirex.pokedex.domain.model.simple.moves.SimpleMove
 import sz.sapphirex.pokedex.domain.model.simple.pokemon.SimplePokemon
 import sz.sapphirex.pokedex.domain.model.utils.DataResult
 import sz.sapphirex.pokedex.domain.use_case.GetPokemonList
@@ -15,8 +19,11 @@ import javax.inject.Inject
 class HomeScreenViewModel @Inject constructor(
     private val getPokemonList: GetPokemonList
 ): ViewModel() {
-    private val _pokemons = MutableStateFlow<DataResult<List<SimplePokemon>>>(DataResult.Loading)
+//    private val _pokemons = MutableStateFlow<DataResult<List<SimplePokemon>>>(DataResult.Loading)
+    private val _pokemons = MutableStateFlow<DataResult<PagingData<SimplePokemon>>>(DataResult.Loading)
     val pokemons = _pokemons.asStateFlow()
+
+    val pagedData: Flow<PagingData<SimplePokemon>> = getPokemonList.invoke().cachedIn(viewModelScope)
 
     init {
         loadPokemons()
@@ -25,7 +32,7 @@ class HomeScreenViewModel @Inject constructor(
     private fun loadPokemons() {
         viewModelScope.launch {
             getPokemonList.invoke().collect { result ->
-                _pokemons.value = result
+                _pokemons.value = DataResult.Success(result)
             }
         }
     }
