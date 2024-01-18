307f617d5a9ec63ae85d1ee53454589793189b55
Arvy Enriquez <jonarvy11@gmail.com>
2024-01-18 22:17:08 +0800

    update type to color util

--- app/src/main/java/sz/sapphirex/pokedex/presentation/utils/Helpers.kt
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/utils/Helpers.kt
@@ -2,34 +2,45 @@ package sz.sapphirex.pokedex.presentation.utils
 
 import androidx.compose.ui.graphics.Color
 import sz.sapphirex.pokedex.domain.model.base.pokemon.PokemonType
+import sz.sapphirex.pokedex.domain.model.base.utility.NamedAPIResource
 
-fun List<PokemonType>.toColor(): Color {
-    val color: Long = when (this.first().type.name) {
-        "normal" -> 0xFFA8A77A
-        "fire" -> 0xFFEE8130
-        "water" -> 0xFF6390F0
-        "electric" -> 0xFFF7D02C
-        "grass" -> 0xFF7AC74C
-        "ice" -> 0xFF96D9D6
-        "fighting" -> 0xFFC22E28
-        "poison" -> 0xFFA33EA1
-        "ground" -> 0xFFE2BF65
-        "flying" -> 0xFFA98FF3
-        "psychic" -> 0xFFF95587
-        "bug" -> 0xFFA6B91A
-        "rock" -> 0xFFB6A136
-        "ghost" -> 0xFF735797
-        "dragon" -> 0xFF6F35FC
-        "dark" -> 0xFF705746
-        "steel" -> 0xFFB7B7CE
-        "fairy" -> 0xFFD685AD
-        else -> 0xFFFFFFFF
-    }
+private val colorMap = mapOf(
+    "normal" to 0xFFA8A77A,
+    "fire" to 0xFFEE8130,
+    "water" to 0xFF6390F0,
+    "electric" to 0xFFF7D02C,
+    "grass" to 0xFF7AC74C,
+    "ice" to 0xFF96D9D6,
+    "fighting" to 0xFFC22E28,
+    "poison" to 0xFFA33EA1,
+    "ground" to 0xFFE2BF65,
+    "flying" to 0xFFA98FF3,
+    "psychic" to 0xFFF95587,
+    "bug" to 0xFFA6B91A,
+    "rock" to 0xFFB6A136,
+    "ghost" to 0xFF735797,
+    "dragon" to 0xFF6F35FC,
+    "dark" to 0xFF705746,
+    "steel" to 0xFFB7B7CE,
+    "fairy" to 0xFFD685AD,
+)
+
+fun NamedAPIResource.toColor(): Color {
+    val color: Long = colorMap[this.name] ?: 0xFFFFFFFF
     return Color(color)
 }
 
+fun List<PokemonType>.toColor(): Color {
+    val color: Long = colorMap[this.first().type.name] ?: 0xFFFFFFFF
+    return Color(color)
+}
+
+fun String.toMoveNameCase(): String {
+    return this.replace("-", " ").toNameCase()
+}
+
 fun String.toNameCase(): String {
-    return this.split(" ").joinToString(",") {
+    return this.split(" ").joinToString(" ") {
         it.replaceFirstChar(Char::titlecase)
     }
 }
