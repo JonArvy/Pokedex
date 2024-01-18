d82b8150feee68e8277bfce299d64a53d9c16387
Arvy Enriquez <jonarvy11@gmail.com>
2024-01-18 22:16:36 +0800

    implement dto model interface skeleton

--- app/src/main/java/sz/sapphirex/pokedex/data/remote/dto/utility/CommonModelsDto.kt
+++ app/src/main/java/sz/sapphirex/pokedex/data/remote/dto/utility/CommonModelsDto.kt
@@ -2,7 +2,20 @@ package sz.sapphirex.pokedex.data.remote.dto.utility
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import sz.sapphirex.pokedex.data.local.entity.utility.APIResourceEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.DescriptionEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.EffectEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.EncounterEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.FlavorTextEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.GenerationGameIndexEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.MachineVersionDetailEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.NameEntity
 import sz.sapphirex.pokedex.data.local.entity.utility.NamedAPIResourceEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.VerboseEffectEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.VersionEncounterDetailEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.VersionGameIndexEntity
+import sz.sapphirex.pokedex.data.local.entity.utility.VersionGroupFlavorTextEntity
+import sz.sapphirex.pokedex.data.remote.dto.DtoInterface
 import sz.sapphirex.pokedex.domain.model.base.utility.APIResource
 import sz.sapphirex.pokedex.domain.model.base.utility.Description
 import sz.sapphirex.pokedex.domain.model.base.utility.Effect
@@ -20,9 +33,13 @@ import sz.sapphirex.pokedex.domain.model.base.utility.VersionGroupFlavorText
 @Serializable
 data class APIResourceDto(
     val url: String
-) {
-    fun toBase(): APIResource {
+): DtoInterface {
+    override fun toBase(): APIResource {
         return APIResource(url = url)
+    }
+
+    override fun toEntity(): APIResourceEntity {
+        return APIResourceEntity(url = url)
     }
 }
 
@@ -30,11 +47,18 @@ data class APIResourceDto(
 data class DescriptionDto(
     val description: String,
     val language: NamedAPIResourceDto
-) {
-    fun toBase(): Description {
+): DtoInterface {
+    override fun toBase(): Description {
         return Description(
             description = description,
             language = language.toBase()
+        )
+    }
+
+    override fun toEntity(): DescriptionEntity {
+        return DescriptionEntity(
+            description = description,
+            language = language.toEntity()
         )
     }
 }
@@ -43,11 +67,18 @@ data class DescriptionDto(
 data class EffectDto(
     val effect: String,
     val language: NamedAPIResourceDto
-) {
-    fun toBase(): Effect {
+): DtoInterface {
+    override fun toBase(): Effect {
         return Effect(
             effect = effect,
             language = language.toBase()
+        )
+    }
+
+    override fun toEntity(): EffectEntity {
+        return EffectEntity(
+            effect = effect,
+            language = language.toEntity()
         )
     }
 }
@@ -59,14 +90,24 @@ data class EncounterDto(
     @SerialName("condition_values") val conditionValues: List<NamedAPIResourceDto>,
     val chance: Int,
     val method: NamedAPIResourceDto
-) {
-    fun toBase(): Encounter {
+): DtoInterface {
+    override fun toBase(): Encounter {
         return Encounter(
             minLevel = minLevel,
             maxLevel = maxLevel,
             conditionValues = conditionValues.map { it.toBase() },
             chance = chance,
             method = method.toBase()
+        )
+    }
+
+    override fun toEntity(): EncounterEntity {
+        return EncounterEntity(
+            minLevel = minLevel,
+            maxLevel = maxLevel,
+            conditionValues = conditionValues.map { it.toEntity() },
+            chance = chance,
+            method = method.toEntity()
         )
     }
 }
@@ -76,12 +117,20 @@ data class FlavorTextDto(
     @SerialName("flavor_text") val flavorText: String,
     val language: NamedAPIResourceDto,
     val version: NamedAPIResourceDto,
-) {
-    fun toBase(): FlavorText {
+): DtoInterface {
+    override fun toBase(): FlavorText {
         return FlavorText(
             flavorText = flavorText,
             language = language.toBase(),
             version = version.toBase()
+        )
+    }
+
+    override fun toEntity(): FlavorTextEntity {
+        return FlavorTextEntity(
+            flavorText = flavorText,
+            language = language.toEntity(),
+            version = version.toEntity()
         )
     }
 }
@@ -90,11 +139,18 @@ data class FlavorTextDto(
 data class GenerationGameIndexDto(
     @SerialName("game_index") val gameIndex: Int,
     val generation: NamedAPIResourceDto
-) {
-    fun toBase():GenerationGameIndex {
+): DtoInterface {
+    override fun toBase():GenerationGameIndex {
         return GenerationGameIndex(
             gameIndex = gameIndex,
             generation = generation.toBase()
+        )
+    }
+
+    override fun toEntity(): GenerationGameIndexEntity {
+        return GenerationGameIndexEntity(
+            gameIndex = gameIndex,
+            generation = generation.toEntity()
         )
     }
 }
@@ -103,11 +159,18 @@ data class GenerationGameIndexDto(
 data class MachineVersionDetailDto(
     val machine: APIResourceDto,
     @SerialName("version_group") val versionGroup: NamedAPIResourceDto
-) {
-    fun toBase(): MachineVersionDetail {
+): DtoInterface {
+    override fun toBase(): MachineVersionDetail {
         return MachineVersionDetail(
             machine = machine.toBase(),
             versionGroup = versionGroup.toBase()
+        )
+    }
+
+    override fun toEntity(): MachineVersionDetailEntity {
+        return MachineVersionDetailEntity(
+            machine = machine.toEntity(),
+            versionGroup = versionGroup.toEntity()
         )
     }
 }
@@ -116,11 +179,18 @@ data class MachineVersionDetailDto(
 data class NameDto(
     val name: String,
     val language: NamedAPIResourceDto
-) {
-    fun toBase(): Name {
+): DtoInterface {
+    override fun toBase(): Name {
         return Name(
             name = name,
             language = language.toBase()
+        )
+    }
+
+    override fun toEntity(): NameEntity {
+        return NameEntity(
+            name = name,
+            language = language.toEntity()
         )
     }
 }
@@ -129,15 +199,15 @@ data class NameDto(
 data class NamedAPIResourceDto(
     val name: String,
     val url: String
-) {
-    fun toBase(): NamedAPIResource {
+): DtoInterface {
+    override fun toBase(): NamedAPIResource {
         return NamedAPIResource(
             name = name,
             url = url
         )
     }
 
-    fun toEntity(): NamedAPIResourceEntity {
+    override fun toEntity(): NamedAPIResourceEntity {
         return NamedAPIResourceEntity(
             name = name,
             url = url
@@ -150,12 +220,20 @@ data class VerboseEffectDto(
     val effect: String,
     @SerialName("short_effect") val shortEffect: String,
     val language: NamedAPIResourceDto,
-) {
-    fun toBase(): VerboseEffect {
+): DtoInterface {
+    override fun toBase(): VerboseEffect {
         return VerboseEffect(
             effect = effect,
             shortEffect = shortEffect,
             language = language.toBase()
+        )
+    }
+
+    override fun toEntity(): VerboseEffectEntity {
+        return VerboseEffectEntity(
+            effect = effect,
+            shortEffect = shortEffect,
+            language = language.toEntity()
         )
     }
 }
@@ -165,12 +243,20 @@ data class VersionEncounterDetailDto(
     val version: NamedAPIResourceDto,
     @SerialName("max_chance") val maxChance: Int,
     @SerialName("encounter_details") val encounterDetails: List<EncounterDto>
-) {
-    fun toBase(): VersionEncounterDetail {
+): DtoInterface {
+    override fun toBase(): VersionEncounterDetail {
         return VersionEncounterDetail(
             version = version.toBase(),
             maxChance = maxChance,
             encounterDetails = encounterDetails.map { it.toBase() }
+        )
+    }
+
+    override fun toEntity(): VersionEncounterDetailEntity {
+        return VersionEncounterDetailEntity(
+            version = version.toEntity(),
+            maxChance = maxChance,
+            encounterDetails = encounterDetails.map { it.toEntity() }
         )
     }
 }
@@ -179,11 +265,18 @@ data class VersionEncounterDetailDto(
 data class VersionGameIndexDto(
     @SerialName("game_index") val gameIndex: Int,
     val version: NamedAPIResourceDto
-) {
-    fun toBase(): VersionGameIndex {
+): DtoInterface {
+    override fun toBase(): VersionGameIndex {
         return VersionGameIndex(
             gameIndex = gameIndex,
             version = version.toBase()
+        )
+    }
+
+    override fun toEntity(): VersionGameIndexEntity {
+        return VersionGameIndexEntity(
+            gameIndex = gameIndex,
+            version = version.toEntity()
         )
     }
 }
@@ -193,12 +286,20 @@ data class VersionGroupFlavorTextDto(
     val text: String,
     val language: NamedAPIResourceDto,
     @SerialName("version_group") val versionGroup: NamedAPIResourceDto
-) {
-    fun toBase(): VersionGroupFlavorText {
+): DtoInterface {
+    override fun toBase(): VersionGroupFlavorText {
         return VersionGroupFlavorText(
             text = text,
             language = language.toBase(),
             versionGroup = versionGroup.toBase()
+        )
+    }
+
+    override fun toEntity(): VersionGroupFlavorTextEntity {
+        return VersionGroupFlavorTextEntity(
+            text = text,
+            language = language.toEntity(),
+            versionGroup = versionGroup.toEntity()
         )
     }
 }
