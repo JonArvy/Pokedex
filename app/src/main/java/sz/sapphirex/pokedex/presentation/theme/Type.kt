512d7b37eb598a203b83ac137aad83d6b1818bec
Sapphirex <jonarvy11@gmail.com>
2024-06-02 01:03:12 +0800

    Implement the full typography class for the app

--- app/src/main/java/sz/sapphirex/pokedex/presentation/theme/Type.kt
+++ app/src/main/java/sz/sapphirex/pokedex/presentation/theme/Type.kt
@@ -1,6 +1,7 @@
 package sz.sapphirex.pokedex.presentation.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -32,49 +33,175 @@ val PoppinsFontFamily = FontFamily(
     Font(R.font.poppins_thinitalic, FontWeight.Thin, FontStyle.Italic),
 )
 
-//val JosefinSansFontFamily = FontFamily(
-//    // Normal
-//    Font(R.font.josefinsans_regular, FontWeight.Normal),
-//    Font(R.font.josefinsans_bold, FontWeight.Bold),
-//    Font(R.font.josefinsans_extralight, FontWeight.ExtraLight),
-//    Font(R.font.josefinsans_light, FontWeight.Light),
-//    Font(R.font.josefinsans_medium, FontWeight.Medium),
-//    Font(R.font.josefinsans_semibold, FontWeight.SemiBold),
-//    Font(R.font.josefinsans_thin, FontWeight.Thin),
-//    // Italics
-//    Font(R.font.josefinsans_italic, FontWeight.Normal, FontStyle.Italic),
-//    Font(R.font.josefinsans_bolditalic, FontWeight.Bold, FontStyle.Italic),
-//    Font(R.font.josefinsans_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
-//    Font(R.font.josefinsans_lightitalic, FontWeight.Light, FontStyle.Italic),
-//    Font(R.font.josefinsans_mediumitalic, FontWeight.Medium, FontStyle.Italic),
-//    Font(R.font.josefinsans_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
-//    Font(R.font.josefinsans_thinitalic, FontWeight.Thin, FontStyle.Italic)
-//)
+val JosefinSansFontFamily = FontFamily(
+    // Normal
+    Font(R.font.josefinsans_regular, FontWeight.Normal),
+    Font(R.font.josefinsans_bold, FontWeight.Bold),
+    Font(R.font.josefinsans_extralight, FontWeight.ExtraLight),
+    Font(R.font.josefinsans_light, FontWeight.Light),
+    Font(R.font.josefinsans_medium, FontWeight.Medium),
+    Font(R.font.josefinsans_semibold, FontWeight.SemiBold),
+    Font(R.font.josefinsans_thin, FontWeight.Thin),
+    // Italics
+    Font(R.font.josefinsans_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.josefinsans_bolditalic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.josefinsans_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.josefinsans_lightitalic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.josefinsans_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.josefinsans_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.josefinsans_thinitalic, FontWeight.Thin, FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = PoppinsFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        lineHeight = 24.0.sp,
+        letterSpacing = 0.5.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    bodyMedium = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        lineHeight = 20.0.sp,
+        letterSpacing = 0.2.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.0.sp,
+        letterSpacing = 0.4.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    displayLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp,
+        lineHeight = 64.0.sp,
+        letterSpacing = (-0.2).sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    displayMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.0.sp,
+        letterSpacing = 0.0.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    displaySmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.0.sp,
+        letterSpacing = 0.0.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.0.sp,
+        letterSpacing = 0.0.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    headlineMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.0.sp,
+        letterSpacing = 0.0.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.0.sp,
+        letterSpacing = 0.0.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    labelLarge = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.0.sp,
+        letterSpacing = 0.1.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    labelMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.0.sp,
+        letterSpacing = 0.5.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        lineHeight = 16.0.sp,
+        letterSpacing = 0.5.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Normal, // Not sure why this is Normal on material design
+        fontSize = 22.sp,
+        lineHeight = 28.0.sp,
+        letterSpacing = 0.0.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.0.sp,
+        letterSpacing = 0.2.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
+    titleSmall = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.0.sp,
+        letterSpacing = 0.1.sp,
+        fontFamily = PoppinsFontFamily,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        )
+    ),
 )
-
